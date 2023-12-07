b5dcaef39ffef5c4c6f7357df42b1dec4549f457
ghgqwer <93539026+ghgqwer@users.noreply.github.com>
2023-12-07 20:51:08 +0800

    Update all_kyes.py

--- all_kyes.py
+++ all_kyes.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, types, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-bot = Bot(token='6331553602:AAEAILCnp7Kx1SPXaAlgrtt_s9hsa2BmgYk')
+bot = Bot(token='')#your bot token
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
